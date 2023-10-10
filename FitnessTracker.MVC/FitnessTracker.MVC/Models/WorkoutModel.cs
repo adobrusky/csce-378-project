fa09d60c000965bcc2ff7bf27b0403eab04531ef
adobrusky <austindobrusky@gmail.com>
2023-10-09 21:36:08 -0500

    Added WorkoutModel for storing objects in cache and passing to view

---
+++ FitnessTracker.MVC/FitnessTracker.MVC/Models/WorkoutModel.cs
@@ -0,0 +1,16 @@
+﻿namespace FitnessTracker.MVC.Models
+{
+    public class WorkoutModel
+    {
+        string Name { get; set; } = String.Empty;
+        string Category { get; set; } = String.Empty;
+        DateTime? Date { get; set; }
+        List<ExerciseModel> Exercises { get; set; } = new List<ExerciseModel>();
+    }
+
+    public class ExerciseModel
+    {
+        string Name { get; set; } = String.Empty;
+        string MetricToMeasure { get; set; } = String.Empty;
+    }
+}
